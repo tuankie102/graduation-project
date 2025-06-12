f2aae43413c408bc28a52c0a1d0709116940437a
Tuankie102 <Tuankie102>
2025-06-12 12:04:13 +0700

    0.7 version 7 final version - handle ađd new for 4 recent post

--- react-vite-jobhunter/src/components/client/card/post.card.tsx
+++ react-vite-jobhunter/src/components/client/card/post.card.tsx
@@ -19,6 +19,7 @@ import {
 } from "react-router-dom";
 import styles from "styles/client.module.scss";
 import { sfIn } from "spring-filter-query-builder";
+import clientStyles from "@/styles/client.module.scss";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -117,63 +118,65 @@ const PostCard = (props: IProps) => {
               </div>
             </Col>
 
-            {displayPost?.map((item) => {
-              return (
-                item.active && (
-                  <Col span={24} md={12} key={item.id}>
-                    <Card
-                      size="small"
-                      title={null}
-                      hoverable
-                      onClick={() => handleViewDetailPost(item)}
-                    >
-                      <div className={styles["card-job-content"]}>
-                        <div className={styles["card-job-left"]}>
-                          <img
-                            alt="example"
-                            src={`${
-                              import.meta.env.VITE_BACKEND_URL
-                            }/storage/company/${item?.job?.company?.logo}`}
-                          />
-                        </div>
-                        <div className={styles["card-job-right"]}>
-                          <div className={styles["job-title"]}>
-                            {item.title}
-                          </div>
-                          <div className={styles["job-location"]}>
-                            <EnvironmentOutlined style={{ color: "#58aaab" }} />
-                            &nbsp;{getLocationName(item.job?.location || "")}
-                          </div>
-                          <div>
-                            <ThunderboltOutlined style={{ color: "orange" }} />
-                            &nbsp;
-                            {(item.job?.salary + "")?.replace(
-                              /\B(?=(\d{3})+(?!\d))/g,
-                              ","
-                            )}{" "}
-                            đ
-                          </div>
-                          <div className={styles["job-apply-count"]}>
-                            <TeamOutlined /> {item.applyCount || 0}
-                            &nbsp;ứng viên / {item.job?.quantity || 0} chỉ tiêu
-                          </div>
-                          <div className={styles["job-updatedAt"]}>
-                            {item.createdAt
-                              ? dayjs(item.createdAt).locale("en").fromNow()
-                              : dayjs(item.createdAt).locale("en").fromNow()}
-                          </div>
-                          <br />
-                          <div className={styles["job-updatedAt"]}>
-                            <UserOutlined />
-                            &nbsp;{item.job?.user?.name || "Unknown"}
-                          </div>
-                        </div>
+            {displayPost?.map((item, index) => (
+              <Col
+                span={24}
+                md={12}
+                key={item.id}
+                style={{ position: "relative" }}
+              >
+                {index < 4 && (
+                  <div className={clientStyles["badge-new"]}>NEW</div>
+                )}
+                <Card
+                  size="small"
+                  title={null}
+                  hoverable
+                  onClick={() => handleViewDetailPost(item)}
+                >
+                  <div className={styles["card-job-content"]}>
+                    <div className={styles["card-job-left"]}>
+                      <img
+                        alt="example"
+                        src={`${
+                          import.meta.env.VITE_BACKEND_URL
+                        }/storage/company/${item?.job?.company?.logo}`}
+                      />
+                    </div>
+                    <div className={styles["card-job-right"]}>
+                      <div className={styles["job-title"]}>{item.title}</div>
+                      <div className={styles["job-location"]}>
+                        <EnvironmentOutlined style={{ color: "#58aaab" }} />
+                        &nbsp;{getLocationName(item.job?.location || "")}
                       </div>
-                    </Card>
-                  </Col>
-                )
-              );
-            })}
+                      <div>
+                        <ThunderboltOutlined style={{ color: "orange" }} />
+                        &nbsp;
+                        {(item.job?.salary + "")?.replace(
+                          /\B(?=(\d{3})+(?!\d))/g,
+                          ","
+                        )}{" "}
+                        đ
+                      </div>
+                      <div className={styles["job-apply-count"]}>
+                        <TeamOutlined /> {item.applyCount || 0}
+                        &nbsp;ứng viên / {item.job?.quantity || 0} chỉ tiêu
+                      </div>
+                      <div className={styles["job-updatedAt"]}>
+                        {item.createdAt
+                          ? dayjs(item.createdAt).locale("en").fromNow()
+                          : dayjs(item.createdAt).locale("en").fromNow()}
+                      </div>
+                      <br />
+                      <div className={styles["job-updatedAt"]}>
+                        <UserOutlined />
+                        &nbsp;{item.job?.user?.name || "Unknown"}
+                      </div>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+            ))}
 
             {(!displayPost || (displayPost && displayPost.length === 0)) &&
               !isLoading && (
